1370014633bedbc3b9c8c45408bd311ac381f6f4
jeison0894 <jeisson0894@gmail.com>
2022-03-07 15:41:05 -0500

    --

--- src/components/DarkModeBtns.jsx
+++ src/components/DarkModeBtns.jsx
@@ -8,9 +8,9 @@ const DarkModeBtns = () => {
   const { texts } = useContext(LanguageContext)
 
   return (
-    <nav className="flex space-x-3 ">
-      <button aria-label='dark Mode' tabIndex="0" onClick={() => activateDarkMode(false)} className="cursor-pointer w-4 h-4 rounded-full bg-neutral-900 border-[1px] border-white"></button>
-      <button aria-label='Ligth Mode' tabIndex="0" onClick={() => activateDarkMode(true)} className="cursor-pointer w-4 h-4 rounded-full bg-white border-[1px] border-black"></button>
+    <nav className="flex space-x-5 ">
+      <button aria-label='dark Mode' tabIndex="0" onClick={() => activateDarkMode(false)} className="cursor-pointer w-5 h-5 rounded-full bg-neutral-900 border-[1px] border-white"></button>
+      <button aria-label='Ligth Mode' tabIndex="0" onClick={() => activateDarkMode(true)} className="cursor-pointer w-5 h-5 rounded-full bg-white border-[1px] border-black"></button>
     </nav >
   )
 }
